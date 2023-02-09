0637b5877dc46f90503674d3d86c4f7b8b7d0c90
Val16eria <KStorer61@icloud.com>
2023-02-09 19:02:58 +0300

    update mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 /* Nav icon */
+document.addEventListener('DOMContentLoaded', () => {
+    const btnMobile = document.querySelector('.mobile-container__menu');
+    const navMobile = document.querySelector('.mobile-container__list');
 
-const navBtn = document.querySelector('.nav__toggle');
-const menuIcon = document.querySelector('.menu-icon');
-const nav = document.querySelector('.nav');
-
-
-navBtn.onclick = function(){
-    nav.classList.toggle('nav--mobile');
-    menuIcon.classList.toggle('menu-icon-active');
-    document.body.classList.toggle('no-scroll');
-};
+    btnMobile.addEventListener('click', () => {
+        navMobile.classList.toggle('mobile-container__list__active');
+    });
+})
